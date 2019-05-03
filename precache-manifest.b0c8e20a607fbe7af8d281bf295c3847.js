3b4e9fa7ff2bac730dbf189a19eeac20a0315890
duongtk <duongtran845@gmail.com>
2019-05-03 10:07:25 +0700

    Updates

---
+++ precache-manifest.b0c8e20a607fbe7af8d281bf295c3847.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e094d9b57c09f68ea2999f0c706444b",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "337222fa71c339905f58",
+    "url": "/reactjs/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/reactjs/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "337222fa71c339905f58",
+    "url": "/reactjs/static/js/main.835346ab.chunk.js"
+  },
+  {
+    "revision": "984e2b0c9d0708ff072f",
+    "url": "/reactjs/static/js/runtime~main.71c88981.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
